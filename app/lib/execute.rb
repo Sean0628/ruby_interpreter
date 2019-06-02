96b5ee344a9900462c1a55a76bddb0aed7aa76f9
Sho Ito <i.sho.0628@gmail.com>
2019-06-02 19:56:11 +0900

    add some buitin functions

--- app/lib/execute.rb
+++ app/lib/execute.rb
@@ -5,7 +5,14 @@ str = minruby_load
 
 tree = minruby_parse(str)
 
-genv = { 'p' => ['builtin', 'p'] }
+genv = {
+  'p' => ['builtin', 'p'],
+  'require' => ['builtin', 'require'],
+  'minruby_parse' => ['builtin', 'minruby_parse'],
+  'minruby_load' => ['builtin', 'minruby_load'],
+  'minruby_call' => ['builtin', 'minruby_call']
+}
+
 lenv = {}
 
 Interp.evaluate(tree, genv, lenv)
