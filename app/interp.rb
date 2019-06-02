c2043f8c189100725a79e49c1bb3f74f8741c90b
Sho Ito <i.sho.0628@gmail.com>
2019-06-02 11:32:04 +0900

    add begin ~ end while func

--- app/interp.rb
+++ app/interp.rb
@@ -42,6 +42,11 @@ def evaluate(tree, env)
     while evaluate(tree[1], env)
       evaluate(tree[2], env)
     end
+  when 'while2'
+    evaluate(tree[2], env)
+    while evaluate(tree[1], env)
+      evaluate(tree[2], env)
+    end
   end
 end
 
